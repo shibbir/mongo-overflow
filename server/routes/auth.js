5130d9e7dcf4b47ff4f1df9aa28c929e64aff001
Shibbir Ahmed <shibbir.io@outlook.com>
2015-03-23 02:54:27 +0600

    cleanup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,20 +3,6 @@ var authentication = require("./middlewares/authentication");
 module.exports = function(app, passport) {
     "use strict";
 
-    app.route("/auth/facebook").get(authentication.requireAnonymous, passport.authenticate("facebook", { scope : "email" }));
-
-    app.route("/auth/facebook/callback").get(authentication.requireAnonymous, passport.authenticate("facebook", {
-        successRedirect: "/",
-        failureRedirect: "/auth/login"
-    }));
-
-    app.route("/auth/google").get(authentication.requireAnonymous, passport.authenticate("google", { scope : ["profile", "email"] }));
-
-    app.route("/auth/google/callback").get(authentication.requireAnonymous, passport.authenticate("google", {
-        successRedirect: "/",
-        failureRedirect: "/auth/login"
-    }));
-
     app.route("/auth/github").get(authentication.requireAnonymous, passport.authenticate("github"));
 
     app.route("/auth/github/callback").get(authentication.requireAnonymous, passport.authenticate("github", {
